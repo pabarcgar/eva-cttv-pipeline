76f851bca5eacd13fc34ea88898ce03099f68d00
Pablo Arce <pablo.arce.garcia@gmail.com>
2017-08-20 23:43:06 +0100

    Added clinvarParser.groovy

---
+++ clinvar_parser/clinvar-groovy-parser/src/main/groovy/uk/ac/ebi/eva/clinvar/clinvarParser.groovy
@@ -0,0 +1,11 @@
+package uk.ac.ebi.eva.clinvar
+
+println "Parsing clinvar ..."
+
+// TODO: read input file name from CLI args
+
+// TODO: create GzipInputStream for input file
+
+// TODO: create XMLClinvarSetIterator, PublicSetParser and Json serializer
+
+// TODO: iterate input file using XMLClinvarSetIterator, transforming using PublicSetParser and serialize to Json
