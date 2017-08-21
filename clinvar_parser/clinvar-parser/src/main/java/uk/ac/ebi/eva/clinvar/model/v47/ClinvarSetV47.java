41746a11a37247145614b8814ff1e68f70582a7e
Pablo Arce <pablo.arce.garcia@gmail.com>
2017-08-21 14:28:28 +0100

    Moved Jaxb generated classes to uk/ac/ebi/eva/clinvar/model/v47/jaxb package. Added new class ClinvarSetV47.java

---
+++ clinvar_parser/clinvar-parser/src/main/java/uk/ac/ebi/eva/clinvar/model/v47/ClinvarSetV47.java
@@ -0,0 +1,8 @@
+package uk.ac.ebi.eva.clinvar.model.v47;
+
+import uk.ac.ebi.eva.clinvar.model.ClinvarSet;
+import uk.ac.ebi.eva.clinvar.model.v47.jaxb.PublicSetType;
+
+public class ClinvarSetV47 { //extends ClinvarSet<PublicSetType> {
+
+}
